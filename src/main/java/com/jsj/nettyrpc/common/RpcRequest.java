2307a980d7ca64665f58645e01ee4d1c49d62810
jiangshenjie <741294595@qq.com>
2018-11-07 21:08:09 +0800

    1107 jsj 小改动

--- src/main/java/com/jsj/nettyrpc/common/RpcRequest.java
+++ src/main/java/com/jsj/nettyrpc/common/RpcRequest.java
@@ -11,8 +11,6 @@ import java.io.Serializable;
  * @author jsj
  * @date 2018-10-9
  */
-@Data
-@NoArgsConstructor
 public class RpcRequest implements Serializable {
 
     private boolean heartBeat;
@@ -36,4 +34,64 @@ public class RpcRequest implements Serializable {
      * 参数
      */
     private Object[] parameters;
+
+    public RpcRequest() {
+    }
+
+    public RpcRequest(boolean heartBeat, Integer requestId, String interfaceName, String methodName, Class<?>[] parameterTypes, Object[] parameters) {
+        this.heartBeat = heartBeat;
+        this.requestId = requestId;
+        this.interfaceName = interfaceName;
+        this.methodName = methodName;
+        this.parameterTypes = parameterTypes;
+        this.parameters = parameters;
+    }
+
+    public boolean isHeartBeat() {
+        return heartBeat;
+    }
+
+    public void setHeartBeat(boolean heartBeat) {
+        this.heartBeat = heartBeat;
+    }
+
+    public Integer getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(Integer requestId) {
+        this.requestId = requestId;
+    }
+
+    public String getInterfaceName() {
+        return interfaceName;
+    }
+
+    public void setInterfaceName(String interfaceName) {
+        this.interfaceName = interfaceName;
+    }
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public void setMethodName(String methodName) {
+        this.methodName = methodName;
+    }
+
+    public Class<?>[] getParameterTypes() {
+        return parameterTypes;
+    }
+
+    public void setParameterTypes(Class<?>[] parameterTypes) {
+        this.parameterTypes = parameterTypes;
+    }
+
+    public Object[] getParameters() {
+        return parameters;
+    }
+
+    public void setParameters(Object[] parameters) {
+        this.parameters = parameters;
+    }
 }
--- src/main/java/com/jsj/nettyrpc/common/RpcResponse.java
+++ src/main/java/com/jsj/nettyrpc/common/RpcResponse.java
@@ -11,8 +11,6 @@ import java.io.Serializable;
  * @author jsj
  * @date 2018-10-9
  */
-@Data
-@NoArgsConstructor
 public class RpcResponse  implements Serializable {
 
     private boolean heartBeat;
@@ -28,4 +26,46 @@ public class RpcResponse  implements Serializable {
      * 错误信息
      */
     private String errorMsg;
+
+    public RpcResponse() {
+    }
+
+    public RpcResponse(boolean heartBeat, Integer requestId, Object serviceResult, String errorMsg) {
+        this.heartBeat = heartBeat;
+        this.requestId = requestId;
+        this.serviceResult = serviceResult;
+        this.errorMsg = errorMsg;
+    }
+
+    public boolean isHeartBeat() {
+        return heartBeat;
+    }
+
+    public void setHeartBeat(boolean heartBeat) {
+        this.heartBeat = heartBeat;
+    }
+
+    public Integer getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(Integer requestId) {
+        this.requestId = requestId;
+    }
+
+    public Object getServiceResult() {
+        return serviceResult;
+    }
+
+    public void setServiceResult(Object serviceResult) {
+        this.serviceResult = serviceResult;
+    }
+
+    public String getErrorMsg() {
+        return errorMsg;
+    }
+
+    public void setErrorMsg(String errorMsg) {
+        this.errorMsg = errorMsg;
+    }
 }
