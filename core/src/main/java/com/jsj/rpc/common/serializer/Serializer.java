d5220b170ca57a9c6c8d9102e2ee18754be19ebf
jiangshenjie <741294595@qq.com>
2019-09-15 14:14:17 +0800

    更新

--- core/src/main/java/com/jsj/rpc/common/serializer/Serializer.java
+++ core/src/main/java/com/jsj/rpc/common/serializer/Serializer.java
@@ -12,11 +12,20 @@ public interface Serializer {
 
     /**
      * 反序列化
+     * @param bytes
+     * @param clazz
+     * @param <T>
+     * @return
+     * @throws SerializationException
      */
     <T> T deSerialize(byte[] bytes, Class<T> clazz) throws SerializationException;
 
     /**
      * 序列化
+     * @param obj
+     * @param <T>
+     * @return
+     * @throws SerializationException
      */
     <T> byte[] serialize(T obj) throws SerializationException;
 }
